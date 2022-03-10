e2cf37b7789616bd2a5128ca9bc25383306b3410
SofiaLiguori <47703033+SofiaLiguori@users.noreply.github.com>
2022-03-10 14:06:27 +0000

    remove useless copy in the np.array(x, dtype=dtype) else branch (#43)
    Co-authored-by: Sofia Liguori <sofial@graphcore.ai>

--- popxl_addons/utils.py
+++ popxl_addons/utils.py
@@ -12,13 +12,13 @@ except ModuleNotFoundError:
 
 def to_numpy(x: HostTensor, dtype=None) -> np.ndarray:
     if torch_imported and isinstance(x, torch.Tensor):
-        x = x.detach().numpy()
+        x = x.detach().numpy().copy()
         if dtype:
             x = x.astype(dtype)
     else:
         x = np.array(x, dtype=dtype)
 
-    return x.copy()
+    return x
 
 
 def suffix_graph_name(name: str, suffix: str):
