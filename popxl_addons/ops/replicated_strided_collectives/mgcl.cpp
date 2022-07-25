8ae7945ca2db0346b19894f4a9512a74ef9e7b88
SofiaLiguori <47703033+SofiaLiguori@users.noreply.github.com>
2022-07-25 12:21:40 +0100

    Revert GCL commit 1b8d2fccc96fe68cf2ab9d19fd7efcf5d0f9772e (#122)
    Co-authored-by: Sofia Liguori <sofial@graphcore.ai>

--- popxl_addons/ops/replicated_strided_collectives/mgcl.cpp
+++ popxl_addons/ops/replicated_strided_collectives/mgcl.cpp
@@ -23,11 +23,11 @@ bool ringSupported(Graph &graph, uint32_t stride, uint32_t size) {
          target.getIpuLinkTopology() == IpuLinkTopology::Torus;
 }
 
-gcl::CommGroup consecutiveGroup(Graph &graph, uint32_t stride, uint32_t size) {
+gcl::CommGroup consecutiveGroup(Graph &graph, uint32_t size) {
   // Handle GCL's preference of ALL
   if (graph.getReplicationFactor() == size)
     return {gcl::CommGroupType::ALL, 0};
-  return {gcl::CommGroupType::CONSECUTIVE, size, stride};
+  return {gcl::CommGroupType::CONSECUTIVE, size};
 }
 
 Tensor maskedAllGather(Graph &graph,
@@ -37,7 +37,7 @@ Tensor maskedAllGather(Graph &graph,
                        uint32_t size) {
   // Gather all Replica's data
   Tensor temp = gcl::allGatherCrossReplica(
-      graph, data, prog, consecutiveGroup(graph, 1, size * stride));
+      graph, data, prog, consecutiveGroup(graph, size * stride));
 
   Tensor replicaIndex = graph.addReplicationIndexConstant({"replicaIndex"});
   graph.setTileMapping(replicaIndex, 1);
@@ -90,7 +90,7 @@ Tensor maskedAllReduce(Graph &graph,
 
   // Gather all Replica's data
   Tensor temp = gcl::allGatherCrossReplica(
-      graph, data, prog, consecutiveGroup(graph, 1, size * stride));
+      graph, data, prog, consecutiveGroup(graph, size * stride));
 
   // Compute GroupId
   Tensor replicaIndex = graph.addReplicationIndexConstant({"replicaIndex"});
@@ -247,6 +247,272 @@ Tensor maskedReduceScatter(Graph &graph,
 
   return scattered;
 }
+
+std::map<unsigned int, unsigned int>
+createRing(const Graph &graph, uint32_t stride, uint32_t size) {
+  unsigned int pod       = graph.getTarget().getIpuLinkDomainSize();
+  unsigned int instances = graph.getReplicationFactor() / pod;
+  assert(stride * size == pod);
+  std::map<unsigned int, unsigned int> ring;
+  for (uint32_t rank = 0; rank < instances; ++rank) {
+    for (uint32_t i = 0; i < 2; ++i) {
+      for (uint32_t j = 0; j < pod; j += 2) {
+        uint32_t offset     = (rank * pod);
+        uint32_t from       = i + j;
+        uint32_t to         = (from + 2) % pod;
+        ring[offset + from] = offset + to;
+      }
+    }
+  }
+  return ring;
+}
+
+std::map<unsigned int, unsigned int>
+reverseMap(const std::map<unsigned int, unsigned int> &ring) {
+  std::map<unsigned int, unsigned int> res;
+  for (const auto &p : ring) {
+    res[p.second] = p.first;
+  }
+  return res;
+}
+
+program::Sequence
+allReduceLoop(Graph &graph,
+              const Tensor &accum,
+              const Tensor &src,
+              const Tensor &src2,
+              const Tensor &dest,
+              const Tensor &dest2,
+              const Tensor &step,
+              const std::map<unsigned int, unsigned int> &ring,
+              const std::map<unsigned int, unsigned int> &revRing,
+              uint32_t stride,
+              bool full) {
+  program::Sequence loop;
+  loop.add(program::CrossReplicaCopy(src, dest, ring, {"CrossReplica1"}));
+  if (full)
+    loop.add(
+        program::CrossReplicaCopy(src2, dest2, revRing, {"CrossReplica2"}));
+  uint32_t halfStride = stride / 2;
+  Tensor strideT      = popops::map(
+      graph, popops::expr::_1 % halfStride, {step}, loop, {"strideIndex"});
+  std::vector<std::pair<std::int32_t, program::Program>> switchBody;
+  program::Sequence sum;
+  if (full)
+    popops::mapInPlace(graph,
+                       popops::expr::_1 + popops::expr::_2 + popops::expr::_3,
+                       {accum, dest, dest2},
+                       sum,
+                       {"sum2"});
+  else
+    popops::addInPlace(graph, accum, dest, sum, {"sum"});
+
+  switchBody.push_back({0, sum});
+  loop.add(program::Switch(strideT, switchBody, {"switchSum"}));
+  loop.add(program::Copy(dest, src));
+  if (full)
+    loop.add(program::Copy(dest2, src2));
+  popops::addInPlace(graph, step, 1U, loop, {"incrementStep"});
+  return loop;
+}
+
+Tensor ringAllReduce(Graph &graph,
+                     const Tensor &data,
+                     program::Sequence &prog,
+                     uint32_t stride,
+                     uint32_t size) {
+  auto ring    = createRing(graph, stride, size);
+  auto revRing = reverseMap(ring);
+
+  // Create Buffers
+  Tensor accum = graph.clone(data, {"accum"});
+  Tensor src   = graph.clone(data, {"source"});
+  Tensor src2  = graph.clone(data, {"source2"});
+  Tensor dst   = graph.clone(data, {"destination"});
+  Tensor dst2  = graph.clone(data, {"destination2"});
+  // Initialise
+  prog.add(program::Copy(data, accum));
+  prog.add(program::Copy(data, src));
+  prog.add(program::Copy(data, src2));
+  prog.add(program::WriteUndef(dst));
+  prog.add(program::WriteUndef(dst2));
+
+  // Create counters
+  Tensor one = graph.addConstant(poplar::UNSIGNED_INT, {1}, 1, {"one"});
+  graph.setTileMapping(one, 1);
+  Tensor step = graph.addVariable(poplar::UNSIGNED_INT, {}, {"index"});
+  graph.setTileMapping(step, 1);
+  prog.add(program::Copy(one, step));
+
+  // Loop
+  program::Sequence loop = allReduceLoop(
+      graph, accum, src, src2, dst, dst2, step, ring, revRing, stride, true);
+  prog.add(program::Repeat((ring.size() / 4) - 1, loop, {"loop"}));
+  // Final Step
+  program::Sequence endLoop = allReduceLoop(
+      graph, accum, src, src2, dst, dst2, step, ring, revRing, stride, false);
+  prog.add(endLoop);
+  return accum;
+}
+
+Tensor ringReduceScatter(Graph &graph,
+                         const Tensor &data,
+                         program::Sequence &prog,
+                         uint32_t stride,
+                         uint32_t size) {
+  assert(data.rank() == 1);
+
+  Tensor reduced_data = ringAllReduce(graph, data, prog, stride, size);
+  Tensor scattered =
+      reduceScatterSlice(graph, reduced_data, prog, stride, size);
+
+  return scattered;
+}
+
+program::Sequence
+allGatherLoop(Graph &graph,
+              const Tensor &res,
+              const Tensor &src,
+              const Tensor &src2,
+              const Tensor &dest,
+              const Tensor &dest2,
+              const Tensor &step,
+              const Tensor &index1,
+              const Tensor &index2,
+              const std::map<unsigned int, unsigned int> &ring,
+              const std::map<unsigned int, unsigned int> &revRing,
+              uint32_t stride,
+              uint32_t group,
+              bool full) {
+  program::Sequence loop;
+  loop.add(program::CrossReplicaCopy(src, dest, ring, {"CrossReplica1"}));
+  if (full)
+    loop.add(
+        program::CrossReplicaCopy(src2, dest2, revRing, {"CrossReplica2"}));
+  uint32_t halfStride = stride / 2;
+  Tensor strideT      = popops::map(
+      graph, popops::expr::_1 % halfStride, {step}, loop, {"strideIndex"});
+  std::vector<std::pair<std::int32_t, program::Program>> switchBody;
+  program::Sequence copies;
+  if (full) {
+    popops::mapInPlace(graph,
+                       (popops::expr::_1 + 1) % group,
+                       {index2},
+                       copies,
+                       {"decrementIndex2"});
+    popops::dynamicUpdate(graph,
+                          res,
+                          dest2.expand({0}),
+                          index2.expand({0}),
+                          {0},
+                          {1},
+                          copies,
+                          {"copyBackward"});
+  }
+  popops::mapInPlace(graph,
+                     (popops::expr::_1 - 1) % group,
+                     {index1},
+                     copies,
+                     {"indcrementIndex1"});
+  popops::dynamicUpdate(graph,
+                        res,
+                        dest.expand({0}),
+                        index1.expand({0}),
+                        {0},
+                        {1},
+                        copies,
+                        {"copyForward"});
+
+  switchBody.push_back({0, copies});
+  loop.add(program::Switch(strideT, switchBody, {"switchCopies"}));
+  loop.add(program::Copy(dest, src));
+  if (full)
+    loop.add(program::Copy(dest2, src2));
+  popops::addInPlace(graph, step, 1U, loop, {"incrementStep"});
+  return loop;
+}
+
+Tensor ringAllGather(Graph &graph,
+                     const Tensor &data,
+                     program::Sequence &prog,
+                     uint32_t stride,
+                     uint32_t size) {
+  auto ring    = createRing(graph, stride, size);
+  auto revRing = reverseMap(ring);
+
+  // Calculate index
+  Tensor replicaIndex = graph.addReplicationIndexConstant({"replicaIndex"});
+  graph.setTileMapping(replicaIndex, 1);
+  Tensor index1 = popops::map(graph,
+                              (popops::expr::_1 / stride),
+                              {replicaIndex},
+                              prog,
+                              {"computeIndex"});
+  Tensor index2 = poputil::duplicate(graph, index1, prog);
+
+  // Create buffers
+  Tensor res  = graph.cloneN(data, size);
+  Tensor src  = graph.clone(data);
+  Tensor src2 = graph.clone(data);
+  Tensor dst  = graph.clone(data);
+  Tensor dst2 = graph.clone(data);
+
+  // Initialise
+  popops::dynamicUpdate(graph,
+                        res,
+                        data.expand({0}),
+                        index1.expand({0}),
+                        {0},
+                        {1},
+                        prog,
+                        {"initialCopy"});
+  prog.add(program::Copy(data, src));
+  prog.add(program::Copy(data, src2));
+  prog.add(program::WriteUndef(dst));
+  prog.add(program::WriteUndef(dst2));
+
+  // Setup counters
+  Tensor one = graph.addConstant(poplar::UNSIGNED_INT, {1}, 1, {"one"});
+  graph.setTileMapping(one, 1);
+  Tensor step = graph.addVariable(poplar::UNSIGNED_INT, {}, {"step"});
+  graph.setTileMapping(step, 1);
+  prog.add(program::Copy(one, step));
+
+  // Loop
+  program::Sequence loop = allGatherLoop(graph,
+                                         res,
+                                         src,
+                                         src2,
+                                         dst,
+                                         dst2,
+                                         step,
+                                         index1,
+                                         index2,
+                                         ring,
+                                         revRing,
+                                         stride,
+                                         size,
+                                         true);
+  prog.add(program::Repeat((ring.size() / 4) - 1, loop, {"loop"}));
+
+  // Final Step
+  program::Sequence endLoop = allGatherLoop(graph,
+                                            res,
+                                            src,
+                                            src2,
+                                            dst,
+                                            dst2,
+                                            step,
+                                            index1,
+                                            index2,
+                                            ring,
+                                            revRing,
+                                            stride,
+                                            size,
+                                            false);
+  prog.add(endLoop);
+  return res;
+}
 } // namespace
 
 // -------- All Reduce --------
@@ -265,31 +531,38 @@ Tensor allReduceStrided(Graph &graph,
         "reduceScatterStrided only supports "
         "gcl::CollectiveOperator::ADD and gcl::CollectiveOperator::MEAN");
   }
-  if (stride == graph.getTarget().getIpuLinkDomainSize()) {
-    return gcl::allReduceCrossReplica(
-        graph,
-        data,
-        op,
-        prog,
-        gcl::CommGroup(gcl::CommGroupType::ORTHOGONAL, size),
-        debugContext,
-        options);
-  }
-  if (stride == 1 || ringSupported(graph, stride, size)) {
+  if (stride == 1) {
     return gcl::allReduceCrossReplica(graph,
                                       data,
                                       op,
                                       prog,
-                                      consecutiveGroup(graph, stride, size),
+                                      consecutiveGroup(graph, size * stride),
                                       debugContext,
                                       options);
+  } else {
+    if (stride == graph.getTarget().getIpuLinkDomainSize()) {
+      return gcl::allReduceCrossReplica(
+          graph,
+          data,
+          op,
+          prog,
+          gcl::CommGroup(gcl::CommGroupType::ORTHOGONAL, size),
+          debugContext,
+          options);
+    } else {
+      Tensor out;
+      if (ringSupported(graph, stride, size)) {
+        out = ringAllReduce(graph, data, prog, stride, size);
+      } else {
+        out = maskedAllReduce(graph, data, prog, stride, size);
+      }
+      if (op == gcl::CollectiveOperator::MEAN) {
+        popops::mulInPlace(
+            graph, out, 1.f / static_cast<float>(size), prog, debugContext);
+      }
+      return out;
+    }
   }
-  Tensor out = maskedAllReduce(graph, data, prog, stride, size);
-  if (op == gcl::CollectiveOperator::MEAN) {
-    popops::mulInPlace(
-        graph, out, 1.f / static_cast<float>(size), prog, debugContext);
-  }
-  return out;
 }
 
 // -------- All Reduce --------
@@ -310,31 +583,39 @@ Tensor reduceScatterStrided(Graph &graph,
         "reduceScatterStrided only supports "
         "gcl::CollectiveOperator::ADD and gcl::CollectiveOperator::MEAN");
   }
-  if (stride == graph.getTarget().getIpuLinkDomainSize()) {
+  if (stride == 1) {
     return gcl::reduceScatterCrossReplica(
         graph,
         data,
         op,
         prog,
-        gcl::CommGroup(gcl::CommGroupType::ORTHOGONAL, size),
+        consecutiveGroup(graph, size * stride),
         debugContext,
         options);
+  } else {
+    if (stride == graph.getTarget().getIpuLinkDomainSize()) {
+      return gcl::reduceScatterCrossReplica(
+          graph,
+          data,
+          op,
+          prog,
+          gcl::CommGroup(gcl::CommGroupType::ORTHOGONAL, size),
+          debugContext,
+          options);
+    } else {
+      Tensor out;
+      if (ringSupported(graph, stride, size)) {
+        out = ringReduceScatter(graph, data, prog, stride, size);
+      } else {
+        out = maskedReduceScatter(graph, data, prog, stride, size);
+      }
+      if (op == gcl::CollectiveOperator::MEAN) {
+        popops::mulInPlace(
+            graph, out, 1.f / static_cast<float>(size), prog, debugContext);
+      }
+      return out;
+    }
   }
-  if (stride == 1 || ringSupported(graph, stride, size)) {
-    return gcl::reduceScatterCrossReplica(graph,
-                                          data,
-                                          op,
-                                          prog,
-                                          consecutiveGroup(graph, stride, size),
-                                          debugContext,
-                                          options);
-  }
-  Tensor out = maskedReduceScatter(graph, data, prog, stride, size);
-  if (op == gcl::CollectiveOperator::MEAN) {
-    popops::mulInPlace(
-        graph, out, 1.f / static_cast<float>(size), prog, debugContext);
-  }
-  return out;
 }
 
 // ------ Reduce Scatter ------
@@ -348,24 +629,30 @@ Tensor allGatherStrided(Graph &graph,
                         uint32_t size,
                         const DebugContext &debugContext,
                         const OptionFlags &options) {
-  if (stride == graph.getTarget().getIpuLinkDomainSize()) {
-    return gcl::allGatherCrossReplica(
-        graph,
-        data,
-        prog,
-        gcl::CommGroup(gcl::CommGroupType::ORTHOGONAL, size),
-        debugContext,
-        options);
-  }
-  if (stride == 1 || ringSupported(graph, stride, size)) {
+  if (stride == 1) {
     return gcl::allGatherCrossReplica(graph,
                                       data,
                                       prog,
-                                      consecutiveGroup(graph, stride, size),
+                                      consecutiveGroup(graph, size * stride),
                                       debugContext,
                                       options);
+  } else {
+    if (stride == graph.getTarget().getIpuLinkDomainSize()) {
+      return gcl::allGatherCrossReplica(
+          graph,
+          data,
+          prog,
+          gcl::CommGroup(gcl::CommGroupType::ORTHOGONAL, size),
+          debugContext,
+          options);
+    } else {
+      if (ringSupported(graph, stride, size)) {
+        return ringAllGather(graph, data, prog, stride, size);
+      } else {
+        return maskedAllGather(graph, data, prog, stride, size);
+      }
+    }
   }
-  return maskedAllGather(graph, data, prog, stride, size);
 }
 
 // -------- All Gather --------
