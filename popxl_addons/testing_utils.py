ec25b08a3a7f8d2cb86167bb238be53a48ac7df0
SofiaLiguori <47703033+SofiaLiguori@users.noreply.github.com>
2022-11-14 11:09:10 +0000

    add run module util (#155)
    * add run module util
    
    * updated function
    
    * fix docstring
    
    * make dtype optional
    
    * change constants to variables
    
    * added tests, remove dataclass
    
    * edit
    
    * change

--- popxl_addons/testing_utils.py
+++ popxl_addons/testing_utils.py
@@ -1,8 +1,108 @@
-# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+# Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 from typing import Iterable, Type
 
 from popart._internal import ir as _ir
+import popxl
+from popxl_addons.module import Module
+from popxl.tensor import Variable
+
+from popxl_addons import NamedTensors
+from popxl_addons.ops import host_store
+import numpy as np
+from typing import Union, Callable, Dict, List, Optional
 
 
 def ops_of_type(ops: Iterable[_ir.Op], op_type: Type[_ir.Op]) -> int:
     return len(list(filter(lambda op: isinstance(op, op_type), ops)))
+
+
+class TensorInput:
+    """
+    Wrapper for a numpy array that is meant to be a tensor input of a graph.
+    If a dytpe is specified, the popxl tensor will be created with such dtype,
+    otherwise the dtype will be inferred from the numpy array.
+    Please note that poplar does not support DOUBLE dtype, which is the default numpy
+    dtype.
+    """
+
+    def __init__(self, data: np.ndarray, dtype: Optional[popxl.dtype] = None) -> None:
+        self.data = data
+        self.dtype = dtype
+
+
+def run_module(module: Module,
+               *args,
+               weights: Optional[Callable[[NamedTensors], Dict[Variable, np.ndarray]]] = None,
+               **kwargs) -> List[np.ndarray]:
+    """
+    Run a module with provided args and return graph output data (numpy arrays)
+
+    Args:
+        - module (Module): the `addons.Module` instance that defines the graph
+        - *args: arguments needed to build and call the module. For tensors you can use numpy data wrapped in a TensorInput class.
+                This way they will automatically be converted into popxl variables before feeding them to the model.
+        - weights (optional, Callable[[NamedTensors], Dict[Variable, np.ndarray]]): a callable that takes the graph variables and produces
+                                                                                    a dictionary of their values. Must be specified as a kwarg.
+        - **kwargs: additional kwargs to pass to the model
+
+    A ir is created, inputs marked as TensorInput are turned into popxl variables and the args provided are used to
+    create graph and factories from the module.
+    If weights are provided for the variables, they are copied to IPU before the session is run.
+    The session is run and graph outputs are returned as numpy arrays.
+
+    Usage:
+        input_np = np.random.rand(4)
+        input_torch = torch.Tensor(input_np)
+
+        torch_linear = torch.nn.Linear(4,2)
+        output_torch = torch_linear(input_torch)
+        output_torch = output_torch.detach().numpy()
+
+        input_t = TensorInput(input_np, popxl.float32)
+        popxl_linear = Linear(2)
+
+        def weight_mapping(variables: NamedTensors):
+            return {
+                variables.weight : to_numpy(torch_linear.weight.data).T,
+                variables.bias: to_numpy(torch_linear.bias)
+            }
+
+        output_popxl, = run_module(popxl_linear, input_t, weights=weight_mapping)
+
+        np.testing.assert_allclose(output_popxl, output_torch.reshape(output_popxl.shape),rtol=10e-3)
+
+    Return:
+        List[np.ndarray] : outputs of the module
+    """
+
+    def typed_arg(a):
+        if isinstance(a, TensorInput):
+            dtype = a.dtype
+            if dtype is None:
+                # try infer from numpy
+                dtype = popxl.dtype.as_dtype(a.data)
+            return popxl.variable(a.data, dtype=dtype)
+        else:
+            return a
+
+    ir = popxl.Ir()
+    with ir.main_graph:
+        new_args = [typed_arg(a) for a in args]
+        new_kwargs = {k: typed_arg(v) for k, v in kwargs.items()}
+        facts, graph = module.create_graph(*new_args, **new_kwargs)
+        vars = facts.init()
+        tensor_args = [a for a in new_args if isinstance(a, popxl.Tensor)]
+        tensor_kwargs = {k: a for k, a in new_kwargs.items() if isinstance(a, popxl.Tensor)}
+        outputs = graph.bind(vars).call(*tensor_args, **tensor_kwargs)
+        outs_d2h = []
+        for o in outputs:
+            outs_d2h.append(host_store(o))
+
+    with popxl.Session(ir=ir, device_desc="ipu_hw") as session:
+        if weights:
+            if isinstance(weights, Callable):
+                weights = weights(vars)
+            session.write_variables_data(weights)
+        outs = session.run()
+    np_outs = [outs[d2h] for d2h in outs_d2h]
+    return np_outs
---
+++ tests/unit/test_run_module.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2022 Graphcore Ltd. All rights reserved.
+import numpy as np
+import torch
+import popxl
+from popxl.utils import to_numpy
+from popxl_addons.layers import Linear
+from popxl_addons import NamedTensors
+from popxl_addons.testing_utils import TensorInput, run_module
+from popxl_addons import Module
+
+
+def test_run_module():
+    input_np = np.random.rand(4)
+    input_torch = torch.Tensor(input_np)
+
+    torch_linear = torch.nn.Linear(4, 2)
+    output_torch = torch_linear(input_torch)
+    output_torch = output_torch.detach().numpy()
+
+    input_t = TensorInput(input_np, popxl.float32)
+    popxl_linear = Linear(2)
+
+    def weight_mapping(variables: NamedTensors):
+        return {variables.weight: to_numpy(torch_linear.weight.data.T), variables.bias: to_numpy(torch_linear.bias)}
+
+    output_popxl, = run_module(popxl_linear, input_t, weights=weight_mapping)
+
+    np.testing.assert_allclose(output_popxl, output_torch.reshape(output_popxl.shape), rtol=10e-3)
+
+
+def test_run_module_in_place():
+    input_np = np.random.rand(4)
+    add_np = np.ones(4)
+
+    input_torch = torch.Tensor(input_np)
+    input_torch += torch.Tensor(add_np)
+
+    input_t = TensorInput(input_np, popxl.float32)
+    add_t = TensorInput(add_np, popxl.float32)
+
+    class InPlaceAdd(Module):
+        @popxl.in_sequence(True)
+        def build(self, x: popxl.TensorByRef, a: popxl.Tensor):
+            popxl.ops.add_(x, a)
+            return x
+
+    popxl_layer = InPlaceAdd()
+
+    output_popxl, = run_module(popxl_layer, input_t, add_t)
+
+    np.testing.assert_allclose(output_popxl, input_torch, rtol=10e-3)
+
+
+if __name__ == '__main__':
+    test_run_module()
+    test_run_module_in_place()
