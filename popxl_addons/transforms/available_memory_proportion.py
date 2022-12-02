aeacc174ab469b37540c73977132f673739b19d9
Ian <31828525+HalestormAI@users.noreply.github.com>
2022-12-02 15:34:14 +0000

    Set AMP for ops using ConvOptions and lack a direct setter (#162)
    * Set AMP for ops using ConvOptions and lack a direct setter
    
    * Fixed other case and removed code dup
    
    * Ran linter

--- popxl_addons/transforms/available_memory_proportion.py
+++ popxl_addons/transforms/available_memory_proportion.py
@@ -1,12 +1,25 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
-from typing import Iterable
+from typing import Dict, Iterable
 
 import popxl
+import popart._internal.ir as _ir
 from popxl.ops.utils import convert_optional_float
 import popxl_addons as addons
 from popxl_addons.graph import GraphWithNamedArgs
 
 __all__ = ["set_available_memory_proportion_by_ipu", "set_graph_available_memory_proportion_by_ipu"]
+
+
+def _set_available_memory_for_op(op: _ir.Op, ipu_to_prop: Dict[int, float]):
+    if hasattr(op, "setAvailableMemoryProportion"):
+        prop = ipu_to_prop.get(op.getVirtualGraphId(), ipu_to_prop[0])
+        op.setAvailableMemoryProportion(convert_optional_float(prop))
+
+    elif hasattr(op, "getConvOptions") and hasattr(op, "setConvOptions"):
+        prop = ipu_to_prop.get(op.getVirtualGraphId(), ipu_to_prop[0])
+        conv_opts = op.getConvOptions()
+        conv_opts.availableMemoryProportions = [prop]
+        op.setConvOptions(conv_opts)
 
 
 def set_available_memory_proportion_by_ipu(ir: popxl.Ir, proportions: Iterable[float]):
@@ -23,9 +36,7 @@ def set_available_memory_proportion_by_ipu(ir: popxl.Ir, proportions: Iterable[f
     ipu_to_prop = dict(enumerate(proportions))
     for g in ir._pb_ir.getAllGraphs():
         for op in g.getOps():
-            if hasattr(op, "setAvailableMemoryProportion"):
-                prop = ipu_to_prop.get(op.getVirtualGraphId(), ipu_to_prop[0])
-                op.setAvailableMemoryProportion(convert_optional_float(prop))
+            _set_available_memory_for_op(op, ipu_to_prop)
 
 
 def set_graph_available_memory_proportion_by_ipu(graph: popxl.Graph, proportions: Iterable[float]):
@@ -51,8 +62,6 @@ def _set_graph_available_memory_proportion_by_ipu(pb_graph, proportions: Iterabl
             if called not in visited_graphs:
                 _set_graph_available_memory_proportion_by_ipu(called, proportions, visited_graphs)
 
-        if hasattr(op, "setAvailableMemoryProportion"):
-            prop = ipu_to_prop.get(op.getVirtualGraphId(), ipu_to_prop[0])
-            op.setAvailableMemoryProportion(convert_optional_float(prop))
+        _set_available_memory_for_op(op, ipu_to_prop)
 
     visited_graphs.append(pb_graph)
