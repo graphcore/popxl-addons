e47ecb046a0dad8264ffdd721a9f519ac17b671c
SofiaLiguori <47703033+SofiaLiguori@users.noreply.github.com>
2022-12-05 16:53:06 +0000

    add possibility of bool and int shard over (#163)
    * add possibility of bool and int shard over
    
    * Update popxl_addons/remote.py
    
    Co-authored-by: Josh Levy-Kramer <joshlk@users.noreply.github.com>
    
    * change docstring
    
    * Update popxl_addons/remote.py
    
    Co-authored-by: Josh Levy-Kramer <joshlk@users.noreply.github.com>
    
    * feedback
    
    * fix indent
    
    * add missing import
    
    Co-authored-by: Josh Levy-Kramer <joshlk@users.noreply.github.com>

--- popxl_addons/remote.py
+++ popxl_addons/remote.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 from contextlib import contextmanager
 from multiprocessing.sharedctypes import Value
-from typing import List, Tuple, Union, Optional, Dict
+from typing import List, Tuple, Union, Optional, Dict, Mapping
 from webbrowser import get
 
 import numpy as np
@@ -42,14 +42,17 @@ def named_buffers(tensors: NamedTensors, entries: int = 1) -> NamedRemoteBuffers
 
 def named_variable_buffers(factories: NamedVariableFactories,
                            entries: int = 1,
-                           shard_over_dict: Optional[Dict[str, int]] = None) -> NamedRemoteBuffers:
+                           shard_over_dict: Union[Mapping[str, int], int, bool] = True) -> NamedRemoteBuffers:
     """Create a buffer for each VariableFactory in `factories`. The buffers will have `entries` set.
 
     Args:
         factories (NamedVariableFactories): VariableFactories to create buffers for.
         entries (int, optional): Number of entries of the buffer. Defaults to 1.
-        shard_over_dict (Dict[str, int], optional): by default, sharded buffers will shard in the full replica group.
-                                                        If you want to shard over less devices, you can provide an entry in this dictionary
+        shard_over_dict ( Union[Dict[str, int], int, bool], optional):  The number of IPUs to shard the tensor over in the replica group of the variable.
+                                                                        For example, if the replica group is of size 4 and shard_over is 2, tensors will be
+                                                                        sharded only on 2 ipus. A shard_over value of 1 implies no sharding.
+                                                                        By default, sharded buffers will shard in the full replica group.
+                                                                        If you don't want any sharding you can either set shard_over to False or 1.
     Returns:
         NamedRemoteBuffers: A buffer for each factory with names matching the NamedVariableFactories' names.
     """
@@ -57,10 +60,21 @@ def named_variable_buffers(factories: NamedVariableFactories,
     shard_over_dict = shard_over_dict if shard_over_dict else {}
     for name, f in factories.to_dict().items():
         tensor_spec = popxl.TensorSpec(shape=f.shape, dtype=f.dtype, meta_shape=f.meta_shape)
+        if isinstance(shard_over_dict, Mapping):
+            shard_over = shard_over_dict.get(name)
+        elif isinstance(shard_over_dict, bool):
+            shard_over = 1 if shard_over_dict == False else f.replica_grouping.group_size
+        elif isinstance(shard_over_dict, int):
+            if shard_over_dict < 1 or shard_over_dict > f.replica_grouping.group_size:
+                raise ValueError(f"Invalid shard_over_dict int value: {shard_over_dict}")
+            shard_over = shard_over_dict
+        else:
+            raise ValueError("shard_over_dict must be a dictionary, an integer or a boolean")
+
         buffers[name] = create_remote_buffer(tensor_spec,
                                              entries=entries,
                                              replica_group=f.replica_grouping,
-                                             shard_over=shard_over_dict.get(name))
+                                             shard_over=shard_over)
     return NamedRemoteBuffers.from_dict(buffers)
 
 
